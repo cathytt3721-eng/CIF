9ffa419219117e53ddbb7e0c548407ce27d7748b
Jayden Thai <cathytt3721@gmail.com>
2025-11-08 22:37:39 -0500

    Add game instructions to Homework4 part 2.py

---
+++ Homework4 part 2.py
@@ -0,0 +1,12 @@
+print("------------------------------------------")
+print("\t How to Play")
+print("------------------------------------------")
+
+
+print("At the start of the you will get 3 lives")
+print("In the game you need to answer 20 question right to win")
+print("When you answer the question right, you will get a item that you can used")
+print("You get three iitem spaces to store item that you can use later")
+print("But if you answer a question wrong, you lose one live.")
+
+print("Good luck" )
